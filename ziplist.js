8c9a1156219c61083e6906b9f082f5db181e3fa9
daomcgill <77309217+daomcgill@users.noreply.github.com>
2023-09-17 21:24:48 -1000

    Finished in 22 mins

---
+++ ziplist.js
@@ -0,0 +1,16 @@
+const _ = require('underscore');
+
+function zipList(list1, list2) {
+  const result = [];
+  for (let i = 0; i < list1.length; i++) {
+    result.push(list1[i], list2[i]);
+  }
+  return result;
+}
+function zipListTheSimpleWay(list1, list2) {
+  return _.flatten(_.zip(list1, list2));
+}
+const list1 = ['a', 'b', 'c'];
+const list2 = [1, 2, 3];
+console.log(zipList(list1, list2));
+console.log(zipListTheSimpleWay(list1, list2));
